a3884f43f8688799d756b0c416a196f3d9376b4a
zlj606 <zhanglijian606@gmail.com>
2014-05-16 22:12:17 +0800

    modify test

--- js/modify.js
+++ js/modify.js
@@ -1,15 +1,48 @@
 $(function(){
-	$("#page").pagination({
-        displayedPages: 10,
-        edges: 1,
-        items: 200,
-        itemsOnPage: 10,
-        onPageClick: function(pageNum, events) {
-            //alert(pageNum + events);
-            //$('.test>label').text('Current Page Number:' + pageNum);
-        },
-        onInit: function() {
-            //$('.test>label').text('Current Page Number:' + 1);
+	
+    
+
+    function addFlower(url, name, fid) {
+        var html = '<div class="col-sm-3 col-lg-3">
+                  <img src="' + url + '" name="' + name + '" fid="' + fid + '">
+                  <p><a class="btn btn-default btn-detail" href="update.html" role="button">View details &raquo;</a></p>
+                </div>';
+        $('.row').append(html);
+    }
+
+    function callBack(jData) {
+        if ("true" != jData.ret) {
+            alert("暂无花种数据！");
+            return;
         }
+        var fArray = jData.res;
+        Util.showPage(fArray.length, 5);
+        for (var i = 0; i < fArray.length; ++i) {
+            addFlower(fArray[i].url, fArray[i].name, fArray[i].fid);
+        }
+
+    }
+    var breed = "1",
+        series = "1",
+        color = "bred",
+        page = "1";
+
+    var data = {
+        "controller": "flower",
+        "action": "query_flower_list",
+        "breed": breed,
+        "series": series,
+        "color" : color,
+        "page" : page
+    };
+    Util.get(url, data, callBack);
+
+    $('.btn-detail').on('click', function(e) {
+        e.preventDefault();
+        sessionStorage.setItem("fid", $(this).attr("fid"));
+        sessionStorage.setItem("name", $(this).attr("name"));
+        sessionStorage.setItem("url", $(this).attr("url"));
+
+        window.location.href = $(this).attr('href');
     });
 });
